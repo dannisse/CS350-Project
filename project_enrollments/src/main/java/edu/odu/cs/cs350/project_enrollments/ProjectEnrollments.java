85782ebb1283c7ea45b967bccf46b2b76c357b2d
areynol <areyn009@odu.edu>
2021-03-24 21:48:40 -0400

    IOException for dates.txt
    Finding Files in directory path

--- project_enrollments/src/main/java/edu/odu/cs/cs350/project_enrollments/ProjectEnrollments.java
+++ project_enrollments/src/main/java/edu/odu/cs/cs350/project_enrollments/ProjectEnrollments.java
@@ -1,9 +1,28 @@
 package edu.odu.cs.cs350.project_enrollments;
 
+import java.io.*;
+import java.util.Scanner;
+
+
+
 public class ProjectEnrollments {
 
-	public static void main(String[] args) {
-		System.out.println("test");
+	public static void main(String args[]) throws Exception{
+		String path = args[0];
+		
+		if(FileImports.exception(path) == false) {
+			FileImports.findFile(path);
+		
+	    /*Scanner sc = new Scanner(new File(path + "2016-02-01.csv"));
+	    sc.useDelimiter(",");
+
+	    while(sc.hasNext()) {
+	        String s = sc.next() + ",";
+	        System.out.print(s);
+	    }
+
+	    sc.close();*/
+	}
 	}
 
 }
