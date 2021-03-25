96245293f578b01e5ca16a54f8190416ecb0d0c5
alovitz <alovi004@odu.edu>
2021-03-24 20:44:59 -0400

    scan csv

--- project_enrollments/src/main/java/edu/odu/cs/cs350/project_enrollments/ProjectEnrollments.java
+++ project_enrollments/src/main/java/edu/odu/cs/cs350/project_enrollments/ProjectEnrollments.java
@@ -1,9 +1,23 @@
 package edu.odu.cs.cs350.project_enrollments;
 
+import java.io.*;
+import java.util.Scanner;
+import java.util.Vector;
+
 public class ProjectEnrollments {
 
-	public static void main(String[] args) {
-		System.out.println("test");
+
+	public static void main(String[] args) throws Exception{
+	    Scanner sc = new Scanner(new File(".csv file"));
+	    sc.useDelimiter(",");
+
+	    while(sc.hasNext()) {
+	        String s = sc.next() + ",";
+	        System.out.print(s);
+	    }
+
+	    sc.close();
 	}
+
 
 }
