3f588ba8436753ffa301461c00c8de6885bb88fd
ccarroll <ccarr025@odu.edu>
2021-03-24 23:27:56 -0400

    fix breaking import when path args don't end in a / or \

--- project_enrollments/src/main/java/edu/odu/cs/cs350/project_enrollments/FileImports.java
+++ project_enrollments/src/main/java/edu/odu/cs/cs350/project_enrollments/FileImports.java
@@ -2,11 +2,29 @@ package edu.odu.cs.cs350.project_enrollments;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class FileImports {
+	
+	// If a path string doesn't end in a / or \, add a / to the end of it.
+	public static String sanitizePath(String path) {
+		//System.out.println("PATH=" + path);
+		if (path.length() == 0) {
+			System.exit(1);
+		}
+		if (!path.endsWith("/") && !path.endsWith("\\")) {
+			path = path.concat("/");
+			//System.out.println("concatted");
+			//System.out.println(path);
+		}
+		//int lastCharIndex = path.length()-1;
+		return path;
+	}
+	
 	public static void findFile(String path) {	
-	    File directoryPath = new File(path);	    
+		File directoryPath = new File(path);	    
 	    File filesList[] = directoryPath.listFiles();
 	    Scanner sc = null;
 	    for(File file : filesList) {
@@ -20,9 +38,7 @@ public class FileImports {
 	    			input = sc.nextLine();
 	                sb.append(input+" ");
 	    	    }
-
 	    	    sc.close();
-
 	    }
 	}
 	public static boolean exception(String path) {
