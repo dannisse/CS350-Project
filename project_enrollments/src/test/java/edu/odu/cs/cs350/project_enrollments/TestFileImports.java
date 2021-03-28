e0f931fdacf2f5cb529a2b772581a1578f6f2390
darenas <daren003@odu.edu>
2021-03-28 03:51:30 -0400

    testFileImports - function examples

--- project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/TestFileImports.java
+++ project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/TestFileImports.java
@@ -2,9 +2,35 @@ package edu.odu.cs.cs350.project_enrollments;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Before;
+import org.junit.Test;    
 
 public class TestFileImports {
+
+	//Exmaple code
+	/*
+	 public class ReadFileTest {
+
+    private Path workingDir;
+
+    @Before
+    public void init() {
+        this.workingDir = Path.of("", "src/test/resources");
+    }
+
+    @Test //example function
+    public void read() throws IOException {
+        Path file = this.workingDir.resolve("test.file");
+        String content = Files.readString(file);
+        assertThat(content, is("duke"));
+    }
+
+}    */
 
 	
 	
