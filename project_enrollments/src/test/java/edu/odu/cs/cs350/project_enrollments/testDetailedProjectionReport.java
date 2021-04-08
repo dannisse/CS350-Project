5610556e9f68fd7550ccdd4580b7b6dfdd540e5b
darenas <daren003@odu.edu>
2021-04-07 22:46:29 -0400

    @before

--- project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/testDetailedProjectionReport.java
+++ project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/testDetailedProjectionReport.java
@@ -1,19 +1,17 @@
 package edu.odu.cs.cs350.project_enrollments;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.hamcrest.Matcher;
-import org.junit.Before;
-
-import org.junit.jupiter.api.Test;
 
 class testDetailedProjectionReport {
+
+	//testDetailedProjectionReport defaultDetailedProjectionReport;
+	DetailedProjecto
+	@Before
+	public void setup()
+	{
+		defaultDetailedProjectionReport  = new testDetailedProjectionReport();
+	}
 
 	@Test
 	void test() {
@@ -24,10 +22,12 @@ class testDetailedProjectionReport {
 
 //shows only lecture classes
 	@Test 
-	void showLecture() {
-	
-	assertThat((testLecture).getLecture, hasItem(hasProperty("Course", is("Lecture"))));
-	}
+	void testShowLecture() {
+		final String testLecture = null;
 
+	//assertThat(showLecture, hasItem(hasProperty("Course", is("Lecture"))));
 	
-}}
+	assertThat(showLecture("Course", is("Lecture")));
+	}
+	
+}
