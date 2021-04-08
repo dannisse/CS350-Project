9591498ab2324064f947b5bb9a21a74a40ec385b
alovitz <alovi004@odu.edu>
2021-04-08 02:29:05 -0400

    testing stuff

--- project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/TestDetailedReport.java
+++ project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/TestDetailedReport.java
@@ -1,7 +1,51 @@
 package edu.odu.cs.cs350.project_enrollments;
 
-import java.io.File;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-public class DetailedReport {
+import org.hamcrest.Matcher;
+import org.junit.Before;
+
+import org.junit.jupiter.api.Test;
+	public class TestDetailedReport {
+
+		DetailedReport defaultDetailedReport;
+		
+		@Before
+		public void setup()
+		{
+			defaultDetailedReport  = new DetailedReport();
+		}
+
+
+
+	//shows only lecture classes
+		@Test 
+		public void testShowLecture() {
+			final String testLecture = null;
+
+		//assertThat(showLecture, hasItem(hasProperty("Course", is("Lecture"))));
+		
+		//assertThat(showLecture("Course", is("Lecture")));
+		}
+		
+		//test for the output
+		@Test
+		public void testDreport(){
+			
+			fail("Not yet implemented");
+
+		}
+		
+		
+		
+	}
+//}
+
 	
 	
