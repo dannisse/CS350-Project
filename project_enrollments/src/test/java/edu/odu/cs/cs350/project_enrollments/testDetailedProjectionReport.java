4c2e9e67115bac26b6f543dee265874fa3bc6b24
darenas <daren003@odu.edu>
2021-04-07 21:54:54 -0400

    added object variables to test (?)

--- project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/testDetailedProjectionReport.java
+++ project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/testDetailedProjectionReport.java
@@ -1,6 +1,15 @@
 package edu.odu.cs.cs350.project_enrollments;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.Matcher;
+import org.junit.Before;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,8 +23,24 @@ class testDetailedProjectionReport {
 }
 
 //shows only lecture classes
-	@test
-	public void testLecture
-	
-	assertThat(testLecture.getLecture(),
-          hasItem(hasProperty("Course", is("Lecture"))));
+	@Test
+	public void testLecture() {
+	Object getLecture;
+	Object testLecture;
+	assertThat((testLecture).getLecture(), hasItem(hasProperty("Course", is("Lecture"))));
+	}
+
+	private Matcher hasItem(Object hasProperty) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Object hasProperty(String string, Object object) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+	private Object is(String string) {
+	// TODO Auto-generated method stub
+	return null;
+}}
