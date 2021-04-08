344732cc0e0c1d99614ae65dc0680140dc473db1
alovitz <alovi004@odu.edu>
2021-04-08 02:14:13 -0400

    added dannisse's detaildreport to this branch

---
+++ project_enrollments/src/main/java/edu/odu/cs/cs350/project_enrollments/DetailedReport.java
@@ -0,0 +1,8 @@
+package edu.odu.cs.cs350.project_enrollments;
+
+import java.io.File;
+
+public class DetailedReport {
+	
+	
+}
---
+++ project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/TestDetailedReport.java
@@ -0,0 +1,7 @@
+package edu.odu.cs.cs350.project_enrollments;
+
+import java.io.File;
+
+public class DetailedReport {
+	
+	
