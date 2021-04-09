ccca61e04f9cdaccf34762e746bf2667f57de504
areynol <areyn009@odu.edu>
2021-04-09 11:35:07 -0400

    Made the tests for the Section class. Still need to test Section.MathFR Contemplating not testing display functions if they are only for debugging purposes

--- project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/TestSection.java
+++ project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/TestSection.java
@@ -2,9 +2,12 @@
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class TestSection {
 
 	@BeforeEach
@@ -15,45 +18,86 @@ class TestSection {
 	void testDefaultConstructor() {
 		Section s1 = new Section();
 		
-		assertTrue(s1.getCourse()== s1.getSubj()+s1.getCrse());
-		assertEquals(s1.getNum(),"");
+		assertEquals(s1.getNum(), 000);
 		assertEquals(s1.getCRNum(), 0);
 		assertEquals(s1.getEnr(), 0);
-		assertEquals(s1.getCap(), 0);
-		assertEquals(s1.getCRN(), 0);
-		assertEquals(s1.getSubj(), 0);
+		assertEquals(s1.getCap(), "");
+		assertEquals(s1.getCRN(), "0");
+		assertEquals(s1.getSubj(), "");
+		assertEquals(s1.getCrse(), "000L");
+		assertEquals(s1.getCourse(), s1.getSubj()+s1.getCrse());
 	}
 
 	@Test
 	void testSectionStringArray() {
-		fail("Not yet implemented");
+		String line = "\"20\",\"13947\",\"CS\",\"120G\",\"INTRO INFO LIT & RSCH REC (A)\",\"0\",\"20\",\"0\",\"H3\",\"1X\",\"C\",\"I\",\"E\","
+				+ "\"SYNC\",\"Y\",\" 545- 635PM\",\"R\",\"WEB5\",\"\",\"O\",\"ZEHRA,S\",\"\",\"20\",\"0\",\"1\",\"SYNC\",\"24-AUG-19\",\"06-DEC-19\","
+				+ "\"0\",\"0\",\"0\",\"RECITATION. YOU MUST ALSO REGISTER FOR THE ONLINE (A) LECTURE\"";
+		String[] fields = line.split("\",\"");
+		Section s1 = new Section(fields);
+			
+		assertEquals(s1.getNum(),120);
+		assertEquals(s1.getCRNum(), Integer.parseInt(fields[1]));
+		assertEquals(s1.getEnr(), Integer.parseInt(fields[7]));
+		assertEquals(s1.getCap(), fields[22]);
+		assertEquals(s1.getCRN(), fields[1]);
+		assertEquals(s1.getSubj(), fields[2]);
+		assertEquals(s1.getCourse(), s1.getSubj()+s1.getCrse());
 	}
 
 	@Test
-	void testSectionSection() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testPrint() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testMathFR() {
-		fail("Not yet implemented");
+	void testSectionCopy() {
+		String line = "\"20\",\"13947\",\"CS\",\"120G\",\"INTRO INFO LIT & RSCH REC (A)\",\"0\",\"20\",\"0\",\"H3\",\"1X\",\"C\",\"I\",\"E\","
+				+ "\"SYNC\",\"Y\",\" 545- 635PM\",\"R\",\"WEB5\",\"\",\"O\",\"ZEHRA,S\",\"\",\"20\",\"0\",\"1\",\"SYNC\",\"24-AUG-19\",\"06-DEC-19\","
+				+ "\"0\",\"0\",\"0\",\"RECITATION. YOU MUST ALSO REGISTER FOR THE ONLINE (A) LECTURE\"";
+		String[] fields = line.split("\",\"");
+		Section s1 = new Section(fields);
+		Section s2 = new Section(s1);
+		
+		assertEquals(s2.getNum(), s1.getNum());
+		assertEquals(s2.getCRNum(), s1.getCRNum());
+		assertEquals(s2.getEnr(), s1.getEnr());
+		assertEquals(s2.getCap(), s1.getCap());
+		assertEquals(s2.getCRN(), s1.getCRN());
+		assertEquals(s2.getSubj(), s1.getSubj());
+		assertEquals(s2.getCourse(), s1.getCourse());
 	}
 
 	@Test
 	void testSetEnr() {
-		fail("Not yet implemented");
+		Section s1 = new Section();
+		
+		s1.setEnr(90);
+		
+		assertEquals(s1.getNum(), 000);
+		assertEquals(s1.getCRNum(), 0);
+		assertEquals(s1.getEnr(), 90);
+		
+		assertEquals(s1.getCap(), "");
+		assertEquals(s1.getCRN(), "0");
+		assertEquals(s1.getSubj(), "");
+		assertEquals(s1.getCrse(), "000L");
+		assertEquals(s1.getCourse(), s1.getSubj()+s1.getCrse());
 	}
 
 	@Test
 	void testSetCap() {
-		//Section s1 = new Section();
-		//getCap();
+		Section s1 = new Section();
 		
+		s1.setCap(90);
+		
+		assertEquals(s1.getNum(), 000);
+		assertEquals(s1.getCRNum(), 0);
+		assertEquals(s1.getEnr(), 0);
+		assertEquals(s1.getCap(), "90");
+		assertEquals(s1.getCRN(), "0");
+		assertEquals(s1.getSubj(), "");
+		assertEquals(s1.getCrse(), "000L");
+		assertEquals(s1.getCourse(), s1.getSubj()+s1.getCrse());
+	}
+	
+	@Test
+	void testMathFR() {
 		fail("Not yet implemented");
 	}
 
@@ -62,4 +106,8 @@ class TestSection {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	void testPrint() {
+		fail("Not yet implemented");
+	}
 }
