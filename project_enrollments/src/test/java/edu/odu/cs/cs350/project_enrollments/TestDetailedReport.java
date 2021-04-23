83537f22060b23818992beaf5d87abecb38db230
darenas <daren003@odu.edu>
2021-04-22 23:40:31 -0400

    nevermind

--- project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/TestDetailedReport.java
+++ project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/TestDetailedReport.java
@@ -29,7 +29,7 @@ import org.junit.jupiter.api.Test;
 			fail("Not yet implemented");
 
 		}
-		
+		/*
 		//TODO test output to see one sheet per course
 		@Test
 	  public void testOneSheet () {
@@ -37,7 +37,10 @@ import org.junit.jupiter.api.Test;
 		}
 	//TODO test output of history of past enrollments
 		@Test
-		
+		  public void testPastEnrollments() {
+				fail("Not yet implemented");
+			}
+		*/
 	
 	}
 	
