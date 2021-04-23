abbb305fd3a77a87121fd404ae176c9fb268b97e
areynol <areyn009@odu.edu>
2021-04-23 02:55:42 -0400

    All of the placeholder test functions are set. May need more. Currently 71.4% of code has been tested.

--- project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/TestSummaryReport.java
+++ project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/TestSummaryReport.java
@@ -3,9 +3,11 @@ package edu.odu.cs.cs350.project_enrollments;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -50,7 +52,32 @@ public class TestSummaryReport {
 	}
 	
 	@Test
-	public void TestgenerateOfferingsAndCourses() {
+	public void TestgenerateOfferingsAndCourses() throws IOException {
+		SummaryReport sr1 = new SummaryReport();
+		
+		Semester currSemester = new Semester();
+		String path = FileImports.sanitizePath("./src/test/resources/201910/201910/");
+		if (FileImports.containsDates(path)) {
+			//todo: need to give semester name and start and end dates
+			
+			ArrayList<File> filesList = FileImports.getFiles(path);
+			for (File f: filesList) {
+				String fileName = f.getName();
+				
+				// Make sure we're not adding dates.txt
+				if(!fileName.equals("dates.txt")) {
+					Snapshot snap = new Snapshot(f);
+					currSemester.addSnapshot(snap);
+				}
+			}
+		}
+		ArrayList<Snapshot> currSnapshots = currSemester.getSnapshots();
+		
+		Snapshot lastCurrSnapshot = currSnapshots.get(currSnapshots.size() - 1);
+		ArrayList<Section> currSections = lastCurrSnapshot.getSections();
+		
+		SortedMap<String, Course > currSemesterList = new TreeMap<String, Course >();
+		sr1.generateOfferingsAndCourses(currSections, currSemesterList);
 		
 		
 		fail("not yet implemented");
