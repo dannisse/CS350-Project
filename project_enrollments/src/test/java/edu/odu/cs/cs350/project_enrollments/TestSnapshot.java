82bcd42b116cc029915c04775b5eb4846f349457
alovitz <alovi004@odu.edu>
2021-04-25 22:26:04 -0400

    fixed snapshot test

--- project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/TestSnapshot.java
+++ project_enrollments/src/test/java/edu/odu/cs/cs350/project_enrollments/TestSnapshot.java
@@ -33,7 +33,9 @@ class TestSnapshot {
 	
 	@Test
 	void testSnapshotFile() {
-		String path = "./src/test/resources/201910/201910/2019-03-31.csv";
+		
+		
+		String path = "./bin/edu/odu/cs/cs350/project_enrollments/resources/201910/201910/2019-03-31.csv";
 		File f = new File(path);
 		
 		Snapshot s = new Snapshot(f);
